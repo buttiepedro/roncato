db36fd0f410a217a3c281135ff83539318714676
Mateo Gurrea <mategurrea@gmail.com>
2026-04-16 11:54:35 -0300

    estilos navegacion

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,12 +1,13 @@
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { useAuth } from "../context/AuthContext.jsx";
-import { useEffect } from "react";
+import { use, useEffect, useState } from "react";
 
 
 
 export default function SideBar() {
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
 
   function handleLogout() {
     logout();
@@ -30,20 +31,23 @@ export default function SideBar() {
       <ul>
         {/* <!-- Items Section --> */}
         {/* Link a Tablero Kanban */}
-        <li className="hover:bg-gray-100">
+        <li className="hover:bg-gray-100 relative group">
           <Link to="/kanban" className="h-16 px-6 flex justify-center items-center w-full focus:text-orange-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 6.878V6a2.25 2.25 0 0 1 2.25-2.25h7.5A2.25 2.25 0 0 1 18 6v.878m-12 0c.235-.083.487-.128.75-.128h10.5c.263 0 .515.045.75.128m-12 0A2.25 2.25 0 0 0 4.5 9v.878m13.5-3A2.25 2.25 0 0 1 19.5 9v.878m0 0a2.246 2.246 0 0 0-.75-.128H5.25c-.263 0-.515.045-.75.128m15 0A2.25 2.25 0 0 1 21 12v6a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 18v-6c0-.98.626-1.813 1.5-2.122" />
             </svg>
           </Link>
+          <span className={`absolute left-15 top-5 bg-blue-500 rounded-2xl px-5 text-white shadow-gray-500 shadow-xs opacity-0 transition-opacity duration-500 group-hover:opacity-100`}>Tablero</span>
+          <span className={`absolute left-0 top-0 h-full w-1 bg-blue-500 transition-opacity ${location.pathname === '/kanban' ? 'opacity-100' : 'opacity-0'}`}></span>
         </li>
         {/* Link a Administracion de usuario*/}
-        <li className="hover:bg-gray-100">
+        <li className="hover:bg-gray-100 relative group">
           <Link to="/admin" className="h-16 px-6 flex justify-center items-center w-full focus:text-orange-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
             </svg>
-
+            <span className={`absolute left-15 top-5 bg-blue-500 rounded-2xl px-5 text-white shadow-gray-500 shadow-xs opacity-0 transition-opacity duration-500 group-hover:opacity-100`}>Administración</span>
+            <span className={`absolute left-0 top-0 h-full w-1 bg-blue-500 transition-opacity ${location.pathname === '/admin' ? 'opacity-100' : 'opacity-0'}`}></span>
           </Link>
         </li>
       </ul>
